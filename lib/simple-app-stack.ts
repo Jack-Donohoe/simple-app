867a024184ffe60594c74f51ab3678904820962b
Jack Donohoe <20093738@mail.wit.ie>
2023-10-09 12:52:54 +0100

    Lambda function to get all movies.

--- lib/simple-app-stack.ts
+++ lib/simple-app-stack.ts
@@ -67,6 +67,22 @@ export class SimpleAppStack extends cdk.Stack {
       }
     );
 
+    const getAllMoviesFn = new lambdanode.NodejsFunction(
+      this,
+      "GetAllMovies",
+      {
+        architecture: lambda.Architecture.ARM_64,
+        runtime: lambda.Runtime.NODEJS_16_X,
+        entry: `${__dirname}/../lambdas/getAllMovies.ts`,
+        timeout: cdk.Duration.seconds(10),
+        memorySize: 128,
+        environment: {
+          TABLE_NAME: moviesTable.tableName,
+          REGION: 'eu-west-1',
+        },
+      }
+    );
+
     const getMovieByIdURL = getMovieByIdFn.addFunctionUrl({
       authType: lambda.FunctionUrlAuthType.NONE,
       cors: {
@@ -74,8 +90,17 @@ export class SimpleAppStack extends cdk.Stack {
       },
     });
 
-    moviesTable.grantReadData(getMovieByIdFn)
+    const getAllMoviesURL = getAllMoviesFn.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+      cors: {
+        allowedOrigins: ["*"],
+      },
+    });
 
+    moviesTable.grantReadData(getMovieByIdFn)
+    moviesTable.grantReadData(getAllMoviesFn)
+
+    new cdk.CfnOutput(this, "Get All Movies Url", { value: getAllMoviesURL.url });
     new cdk.CfnOutput(this, "Get Movie Function Url", { value: getMovieByIdURL.url });
     new cdk.CfnOutput(this, "Simple Function Url", { value: simpleFnURL.url });
   }
